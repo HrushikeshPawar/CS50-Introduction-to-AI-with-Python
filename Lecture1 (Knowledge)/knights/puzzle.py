70dc53da7d30c5fbbe48b9a5bcd8fdcb4e3889f6
HrushikeshPawar <hrushikeshpawar96@gmail.com>
2020-07-04 18:39:28 +0530

    Final Code

--- Lecture1 (Knowledge)/knights/puzzle.py
+++ Lecture1 (Knowledge)/knights/puzzle.py
@@ -9,24 +9,39 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+
+# symbols
+symbols = [[AKnight, AKnave], [BKnight, BKnave], [CKnight, CKnave]]
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+Or(AKnight, AKnave),                            #A is Knight or Knave
+Implication(AKnight, And(AKnight, AKnave)),     #If A is Knight then statement is True
+Implication(AKnave, Not(And(AKnight, AKnave)))  #If A is Knave then statement is False
 )
+
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+Or(AKnight, AKnave),                            #A is Knight or Knave
+Or(BKnight, BKnave),                            #B is Knight or Knave
+Implication(AKnave, Not(And(AKnave, BKnave))),  #If A is Knave then statement is False
+Implication(AKnight, And(AKnave, BKnave))       #If A is Knight then statement is True
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+Or(AKnight, AKnave),                            #A is Knight or Knave
+Or(BKnight, BKnave),                            #B is Knight or Knave
+Implication(AKnave, Not(And(AKnave, BKnave))),  #If A is Knave then statement is False
+Implication(AKnight, And(AKnight, BKnight)),    #If A is Knight then statement is True
+Implication(BKnave, Not(And(AKnight, BKnave))), #If B is Knave then statement is False
+Implication(BKnight, And(BKnight, AKnave))      #If B is Knight then statement is True
 )
 
 # Puzzle 3
@@ -35,7 +50,15 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+Or(AKnight, AKnave),                            #A is Knight or Knave
+Or(BKnight, BKnave),                            #B is Knight or Knave
+Or(CKnight, CKnave),                            #C is Knight or Knave
+Implication(AKnight, Or(AKnight, AKnave)),      #If A is Knight then statement is True
+Implication(AKnave, Not(Or(AKnight, AKnave))),  #If A is Knave then statement is False
+Implication(BKnight, And(AKnave, CKnave)),      #If B is Knight then statements are True
+Implication(BKnave, Not(And(AKnave, CKnave))),  #If B is Knave then statements are False
+Implication(CKnight, AKnight),                  #If C is Knight the statement is True
+Implication(CKnave, Not(AKnight))               #If C is Knave the statement is False
 )
 
 
