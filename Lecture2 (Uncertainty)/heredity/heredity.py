b511543f5bf30fd6ddc63f6ccb61b4e12695afc5
HrushikeshPawar <hrushikeshpawar96@gmail.com>
2020-12-09 20:39:27 +0530

    Project 2b - Implemented update function

--- Lecture2 (Uncertainty)/heredity/heredity.py
+++ Lecture2 (Uncertainty)/heredity/heredity.py
@@ -259,7 +259,25 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    
+    #Update probabilities of each person
+    for person in probabilities:
+
+        #Update the genes probability
+        if person in two_genes:
+            probabilities[person]["gene"][2] = p
+        
+        elif person in one_gene:
+            probabilities[person]["gene"][1] = p
+
+        else:
+            probabilities[person]["gene"][0] = p
+
+        # Update the trait probability
+        if person in have_trait:
+            probabilities[person]["trait"] = p
+        else:
+            probabilities[person]["trait"] = p
 
 
 def normalize(probabilities):
